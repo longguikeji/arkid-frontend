8c27d2295eaaa66ac822c267cbf0f89277256618
雷韩 <leihan@leihandeiMac.local>
2019-11-27 18:46:13 +0800

    fix:fix request

--- src/oneid-app/admin/config/FileStorage.ts
+++ src/oneid-app/admin/config/FileStorage.ts
@@ -7,49 +7,47 @@ import './FileStorage.less'
 
 @Component({
   template: html`
-  <div v-else class="ui-file-config flex-col flex-auto">
-    <div>
-      <div class="main">
-        <div>
-          <h2 class="subtitle">设置文件存储位置</h2>
-        </div>
-        <div>
-          <RadioGroup v-model="storageMethod">
-            <Radio label="local">本地存储</Radio>
-            <Radio class="radio" label="minio">Minio</Radio>
-          </RadioGroup>
-          <Form
-            :label-width="200"
-            style="width: 800px;"
-            :model="minioForm"
-            :rules="minioFormRules"
-            ref="minioForm"
-            v-if="storageMethod === 'minio'"
-          >
-            <FormItem prop="endPoint" label="End Point">
-              <Input placeholder="请输入End Point" v-model="minioForm.endPoint"></Input>
-            </FormItem>
-            <FormItem prop="accessKey" label="Access Key">
-              <Input placeholder="请输入Access Key" v-model="minioForm.accessKey"></Input>
-            </FormItem>
-            <FormItem prop="secretKey" label="Secret Key">
-              <Input placeholder="请输入Secret Key" v-model="minioForm.secretKey"></Input>
-            </FormItem>
-            <FormItem prop="secure" label="Secure">
-              <Checkbox v-model="minioForm.secure"></Checkbox>
-            </FormItem>
-            <FormItem prop="location" label="Location">
-              <Input placeholder="请输入Location" v-model="minioForm.location"></Input>
-            </FormItem>
-            <FormItem prop="bucket" label="Bucket">
-              <Input placeholder="请输入Bucket" v-model="minioForm.bucket"></Input>
-            </FormItem>
-          </Form>
-        </div>
-        <div class="ui-file-config-save">
-          <div class="button-area flex-col">
-            <Button class="save-button" type="primary" :loading="isSaving" @click="checkMinioForm">保存</Button>
-          </div>
+  <div class="ui-file-config flex-col flex-auto">
+    <div class="main">
+      <div>
+        <h2 class="subtitle">设置文件存储位置</h2>
+      </div>
+      <div>
+        <RadioGroup v-model="storageMethod">
+          <Radio label="local">本地存储</Radio>
+          <Radio class="radio" label="minio">Minio</Radio>
+        </RadioGroup>
+        <Form
+          :label-width="200"
+          class="ui-minio-form"
+          :model="minioForm"
+          :rules="minioFormRules"
+          ref="minioForm"
+          v-if="storageMethod === 'minio'"
+        >
+          <FormItem prop="endPoint" label="End Point">
+            <Input placeholder="请输入End Point" v-model="minioForm.endPoint"></Input>
+          </FormItem>
+          <FormItem prop="accessKey" label="Access Key">
+            <Input placeholder="请输入Access Key" v-model="minioForm.accessKey"></Input>
+          </FormItem>
+          <FormItem prop="secretKey" label="Secret Key">
+            <Input placeholder="请输入Secret Key" v-model="minioForm.secretKey"></Input>
+          </FormItem>
+          <FormItem prop="secure" label="Secure">
+            <Checkbox v-model="minioForm.secure"></Checkbox>
+          </FormItem>
+          <FormItem prop="location" label="Location">
+            <Input placeholder="请输入Location" v-model="minioForm.location"></Input>
+          </FormItem>
+          <FormItem prop="bucket" label="Bucket">
+            <Input placeholder="请输入Bucket" v-model="minioForm.bucket"></Input>
+          </FormItem>
+        </Form>
+      </div>
+      <div class="ui-file-config-save">
+        <div class="button-area flex-col">
+          <Button class="save-button" type="primary" :loading="isSaving" @click="doSave">保存</Button>
         </div>
       </div>
     </div>
@@ -83,28 +81,21 @@ export default class FileStorage extends Vue {
     this.storageData = await ConfigApi.getStorageData()
 
     const {method, minio} = this.storageData
-    if (method) {
-      this.storageMethod = method
-    }
+
+    this.storageMethod = method
     this.minioForm = minio
   }
 
-  checkMinioForm() {
-    if (this.storageMethod === 'local') {
-      this.doStorageSave()
-    } else {
-      this.$refs.minioForm.validate( async valid => {
-        if (valid) {
-          this.doStorageSave()
-        }
-      })
+  async doSave() {
+    this.storageData!.method = this.storageMethod
+    if (this.storageMethod === 'minio') {
+      // tslint:disable-next-line: await-promise
+      const valid = await this.$refs.minioForm.validate()
+      if (!valid) {
+        return
+      }
     }
-  }
-
-  async doStorageSave() {
     try {
-      this.storageData!.method = this.storageMethod
-      this.storageData!.minio = this.minioForm
       await ConfigApi.updateStorage(this.storageData!)
       this.$Message.success('保存成功')
     } catch (e) {
--- src/oneid-app/admin/config/index.ts
+++ src/oneid-app/admin/config/index.ts
@@ -1,5 +1,5 @@
-import ConfigLayout from './ConfigLayout'
+import Config from './Config'
 
 export const routes = [
-  {path: '/admin/config', name: 'admin.config', component: ConfigLayout},
+  {path: '/admin/config', name: 'admin.config', component: Config},
 ].map((c) => ({...c, meta: {title: '全部应用', matchNav: 'admin.config'}}))
