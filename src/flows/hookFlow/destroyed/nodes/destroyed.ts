71e59ec8bc00cb84a238b0d791415c84eff86211
longgui-penglei <penglei@longguikeji.com>
2021-04-12 16:53:17 +0800

    fix: 🐛 修改destroyed流内容

--- src/flows/hookFlow/destroyed/nodes/destroyed.ts
+++ src/flows/hookFlow/destroyed/nodes/destroyed.ts
@@ -5,8 +5,8 @@ export class Destroyed extends StateNode {
   async run() {
     // 上一个页面销毁后，应该此时去执行对应类型的下一个页面内容
     const tempState = this.getState()
-    const inputs = tempState.destroyed[0]
-    if (tempState && inputs) {
+    const inputs = tempState?.destroyed?.length && tempState.destroyed[0]
+    if (inputs) {
       if (tempState.type === 'TablePage') {
         await runFlowByFile('flows/tablePage/fetch', inputs)
       } else if (tempState.type === 'FormPage') {
