9a580d6acbec26f5f5ce05691f12ea7c16ad2c7e
longgui-penglei <penglei@longguikeji.com>
2021-04-13 16:06:26 +0800

    refactor: 💡 调整验证token的位置, 在init之前进行token的验证

--- src/flows/init/nodes/verifyToken.ts
+++ src/flows/init/nodes/verifyToken.ts
@@ -3,6 +3,7 @@ import { getToken, removeToken } from "@/utils/auth"
 
 export class VerifyToken extends APINode {
   async run() {
+    // init 初始化时判断此时的 token 是否有效
     const token = getToken()
     if (token) {
       this.url = '/api/v1/user/token/'
