8dbefd9e1057ba777efdd26e84eeffbff4ad4bc0
longgui-penglei <penglei@longguikeji.com>
2021-04-16 10:25:34 +0800

    fix: 🐛 修改next参数获取方式

--- src/login/flows/ButtonClick/nodes/HttpResponse.ts
+++ src/login/flows/ButtonClick/nodes/HttpResponse.ts
@@ -11,9 +11,9 @@ export class HttpResponse extends FunctionNode {
       // login success
       
       // 登录之后进行当前登录地址的判断，如果当前登录地址有next参数，重定向到next中
-      const next = window.location.search.substring(1).match(/(^|&)next=([^&]*)(&|$)/)
+      const next = com.$route.query.next
       if (next) {
-        const redirectUrl = window.location.origin + decodeURIComponent(next[2])
+        const redirectUrl = window.location.origin + next
         window.location.replace(redirectUrl)
         return
       }
