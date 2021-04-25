1a6f42e19ad097a3e135cc68c19ffd2e84b32077
longgui-penglei <penglei@longguikeji.com>
2021-04-25 11:05:16 +0800

    feat: 🎸 添加dashboardPage页面流内容, 开始实现desktop页面

--- src/admin/DashboardPage/DashboardPageState.ts
+++ src/admin/DashboardPage/DashboardPageState.ts
@@ -2,5 +2,7 @@ import DashboardItemState from './DashboardItem/DashboardItemState'
 import { BaseState } from '@/admin/base/BaseVue'
 
 export default interface DashboardPageState extends BaseState {
+  type: string
+  pages: Array<string>
   items?: Array<DashboardItemState>
 }
---
+++ src/flows/dashboardPage/fetch/nodes/changeState.ts
@@ -0,0 +1,37 @@
+import { FunctionNode } from 'arkfbp/lib/functionNode'
+import DashboardItemState from '@/admin/DashboardPage/DashboardItem/DashboardItemState'
+
+export class ChangeState extends FunctionNode {
+  async run() {
+    const state = this.$state.fetch()
+    const apps = this.inputs.data.results // apps
+    let x = 0, y = 0
+    apps.forEach((app, index) => {
+      y = Math.floor(index / 4)
+      const item: DashboardItemState = {
+        type: 'CardPanel',
+        state: {
+          state: {
+            img: app.logo,
+            name: app.name,
+            url: app.url,
+            description: app.description
+          }
+        },
+        position: {
+          x: x,
+          y: y,
+          w: 2,
+          h: 2
+        }
+      }
+      if (x >= 6) {
+        x = 0
+      } else {
+        x = x + 2
+      }
+      state.client.items.push(item)
+    })
+    return this.inputs
+  }
+}
