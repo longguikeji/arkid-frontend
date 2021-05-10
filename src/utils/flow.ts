dc26b877ac5192c797285c3290c3816b80f59398
longgui-penglei <penglei@longguikeji.com>
2021-05-10 18:09:27 +0800

    fix: 🐛 测试treePage页面, 并修复部分内容

--- src/utils/flow.ts
+++ src/utils/flow.ts
@@ -1,16 +1,18 @@
 export function proxyClientServer(clientServer: any, data: any) {
-  Object.keys(clientServer).forEach(key => {
-    const item = clientServer[key]
-    if (typeof item === 'object') {
-      const val = data[item?.value]
-      if (val) {
-        const selectClientServer = {
-          ...item[val]
+  if (clientServer) {
+    Object.keys(clientServer).forEach(key => {
+      const item = clientServer[key]
+      if (typeof item === 'object') {
+        const val = data[item?.value]
+        if (val) {
+          const selectClientServer = {
+            ...item[val]
+          }
+          clientServer[key] = item.value
+          Object.assign(clientServer, selectClientServer)
         }
-        clientServer[key] = item.value
-        Object.assign(clientServer, selectClientServer)
       }
-    }
-  })
-  return clientServer
+    })
+    return clientServer
+  }
 }
