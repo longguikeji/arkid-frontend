8d89d518032f255731b4a4b6744700d9bd55f562
longgui-penglei <penglei@longguikeji.com>
2021-05-11 14:35:34 +0800

    fix: 🐛 修正proxyClientServer方法

--- src/utils/flow.ts
+++ src/utils/flow.ts
@@ -1,18 +1,21 @@
+import { cloneDeep } from 'lodash'
+
 export function proxyClientServer(clientServer: any, data: any) {
-  if (clientServer) {
-    Object.keys(clientServer).forEach(key => {
-      const item = clientServer[key]
+  let newClientServer = cloneDeep(clientServer)
+  if (newClientServer) {
+    Object.keys(newClientServer).forEach(key => {
+      const item = newClientServer[key]
       if (typeof item === 'object') {
         const val = data[item?.value]
         if (val) {
           const selectClientServer = {
             ...item[val]
           }
-          clientServer[key] = item.value
-          Object.assign(clientServer, selectClientServer)
+          newClientServer[key] = item.value
+          Object.assign(newClientServer, selectClientServer)
         }
       }
     })
-    return clientServer
+    return newClientServer
   }
 }
