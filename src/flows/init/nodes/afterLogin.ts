170cc5ea7c051256f9b2ae6957fb57fc8f089f5b
longgui-penglei <penglei@longguikeji.com>
2021-05-29 23:49:19 +0800

    fix: 🐛 将验证token的操作移动到authApiNode中

--- src/flows/init/nodes/afterLogin.ts
+++ src/flows/init/nodes/afterLogin.ts
@@ -1,7 +1,6 @@
 import { AuthApiNode } from '@/nodes/authApiNode'
 import OpenAPI from '@/config/openapi'
 import { UserModule } from '@/store/modules/user'
-import { getToken } from '@/utils/auth'
 import { TenantModule } from '@/store/modules/tenant'
 import processUUId from '@/utils/process-uuid'
 import { getUrlParamByName } from '@/utils/url'
@@ -9,30 +8,27 @@ import { getUrlParamByName } from '@/utils/url'
 export class AfterLogin extends AuthApiNode {
   async run() {
     // 当用户已经登录后进行openAPI的访问，并生成动态路由内容，否则不进行生成
-    const token = getToken()
-    if (token) {
-      // 获取OpenAPI内容
-      await OpenAPI.instance.init('/api/schema?format=json')
+    // 获取OpenAPI内容
+    await OpenAPI.instance.init('/api/schema?format=json')
 
-      // 此时进行tenant的获取
-      if (TenantModule.currentTenant.uuid === undefined) {
-        let tenantUUId = TenantModule?.currentTenant?.uuid || getUrlParamByName('tenant') || getUrlParamByName('tenant_uuid')
-        tenantUUId = processUUId(tenantUUId)
-        this.url = '/api/v1/tenant/'
-        this.method = 'GET'
-        const outputs = await super.run()
-        outputs.results.forEach(output => {
-          if (output.uuid === tenantUUId || outputs.results.length === 1) { 
-            TenantModule.changeCurrentTenant(output)
-          }
-        })
-      }
-
-      // 进行用户信息的获取，包括用户的头像、名称、用户的uuid以及用户的权限
-      this.url = '/api/v1/user/info/'
+    // 此时进行tenant的获取
+    if (TenantModule.currentTenant.uuid === undefined) {
+      let tenantUUId = TenantModule?.currentTenant?.uuid || getUrlParamByName('tenant') || getUrlParamByName('tenant_uuid')
+      tenantUUId = processUUId(tenantUUId)
+      this.url = '/api/v1/tenant/'
       this.method = 'GET'
-      const userInfo = await super.run()
-      UserModule.setUser(userInfo)
+      const outputs = await super.run()
+      outputs.results.forEach(output => {
+        if (output.uuid === tenantUUId || outputs.results.length === 1) { 
+          TenantModule.changeCurrentTenant(output)
+        }
+      })
     }
+
+    // 进行用户信息的获取，包括用户的头像、名称、用户的uuid以及用户的权限
+    this.url = '/api/v1/user/info/'
+    this.method = 'GET'
+    const userInfo = await super.run()
+    UserModule.setUser(userInfo)
   }
 }
--- src/nodes/authApiNode.ts
+++ src/nodes/authApiNode.ts
@@ -1,16 +1,33 @@
 import { APINode } from "arkfbp/lib/apiNode"
-import { getToken } from '@/utils/auth'
+import { getToken, removeToken } from '@/utils/auth'
 import getPageState, { getPreviousPageState, getBaseState } from '@/utils/get-page-state'
 
 export class AuthApiNode extends APINode {
   async run() {
     const token = getToken()
+    const url = this.url
+    const method = this.method
+    const params = this.params
     if (token) {
-      this.headers = {
-        Authorization: 'Token ' + token
+      this.url = '/api/v1/user/token/'
+      this.method = 'POST'
+      this.params = {
+        token: token,
+      }
+      const isValid = await super.run()
+      if (!isValid) {
+        removeToken()
+      } else {
+        this.headers = {
+          Authorization: 'Token ' + token
+        }
+        this.url = url
+        this.method = method
+        this.params = params
+        const outputs = await super.run()
+        return outputs
       }
     }
-    return super.run()
   }
 
   getState(path = '') {
