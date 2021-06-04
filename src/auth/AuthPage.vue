6b98800c3f48c91e5baed316ed6a8fdf7a7b9042
longgui-penglei <penglei@longguikeji.com>
2021-06-04 17:37:44 +0800

    feat: 🎸 初始化AuthPage页面, 两种方案, 一个完全由用户自定义授权页面, 另一个使用自定义授权页面模板

--- src/auth/AuthPage.vue
+++ src/auth/AuthPage.vue
@@ -1,66 +1,83 @@
 <template>
   <div class="auth-page">
     <el-form
-      ref="form"
-      :model="form"
-      label-width="80px"
+      label-width="100px"
     >
-      <template />
-      <template>
+      <el-form-item label="是否使用模板">
+        <el-switch v-model="useTemplate" />
+      </el-form-item>
+      <template v-if="useTemplate">
+        <el-form-item label="查看模板样式">
+          <el-button
+            type="text"
+            @click="showAuthTemplate"
+          >
+            点击打开
+          </el-button>
+        </el-form-item>
         <el-form-item label="图标">
           <el-input
-            v-model="form.icon"
+            v-model="template.icon"
             placeholder="请输入图标"
           />
         </el-form-item>
         <el-form-item label="标题">
           <el-input
-            v-model="form.title"
+            v-model="template.title"
             placeholder="请输入标题"
           />
         </el-form-item>
         <el-form-item label="授权信息">
-          <textarea v-model="form.info" />
+          <Tinymce v-model="template.info" />
         </el-form-item>
         <el-form-item
-          v-for="(item, index) in 2"
+          v-for="(item, index) in btnLabels"
           :key="index"
-          :label="btnLabels[index]"
+          :label="item"
         >
           <el-card>
             <el-form
-              ref="form2"
-              :model="form"
+              :model="template"
               label-width="80px"
             >
               <el-form-item label="文本">
                 <el-input
-                  v-model="form.icon"
+                  v-model="template.btns[index].text"
                   placeholder="请输入按钮文本"
                 />
               </el-form-item>
               <el-form-item label="颜色设置">
                 文字颜色 <el-color-picker
-                  v-model="form.icon"
+                  v-model="template.btns[index].color"
                   class="set-color"
                 />
                 按钮颜色 <el-color-picker
-                  v-model="form.icon"
+                  v-model="template.btns[index].bgcolor"
                   class="set-color"
                 />
               </el-form-item>
               <el-form-item label="长宽设置">
                 长度 <el-input-number
-                  v-model="form.width"
+                  v-model="template.btns[index].width"
                   placeholder="长度"
                 />
                 宽度 <el-input-number
-                  v-model="form.width"
+                  v-model="template.btns[index].height"
                   placeholder="宽度"
                 />
               </el-form-item>
             </el-form>
           </el-card>
+        </el-form-item>
+      </template>
+      <template v-else>
+        <el-form-item label="自定义HTML">
+          <el-input
+            v-model="html"
+            type="textarea"
+            :autosize="{minRows: 20}"
+            placeholder="请输入Html内容"
+          />
         </el-form-item>
       </template>
       <el-form-item>
@@ -72,30 +89,50 @@
         </el-button>
       </el-form-item>
     </el-form>
+    <AuthTemplate ref="authtemplate" />
   </div>
 </template>
 
 <script lang="ts">
-import Vue from 'vue'
-import { Component, Prop, Watch } from 'vue-property-decorator'
+import { Component, Prop, Vue } from 'vue-property-decorator'
 import Tinymce from '@/components/Tinymce/index.vue'
+import AuthTemplate from './AuthTemplate.vue'
+import AuthPageTemplate from './AuthPage'
 import { runWorkflowByClass } from 'arkfbp/lib/flow'
 import { Main as SaveAuthPage } from './flows/SaveAuthPage'
 
 @Component({
   name: 'Auth',
   components: {
-    Tinymce
+    Tinymce,
+    AuthTemplate
   }
 })
 
 export default class extends Vue {
-  content = ''
-  form = {
-    title: '',
+  private btnLabels: Array<string> = ['确认按钮', '取消按钮']
+  private useTemplate = false
+  private html = ''
+  private template: AuthPageTemplate = {
     icon: '',
+    title: '',
     info: '',
-    width: ''
+    btns: [
+      {
+        text: '',
+        bgcolor: '',
+        color: '',
+        width: 0,
+        height: 0
+      },
+      {
+        text: '',
+        bgcolor: '',
+        color: '',
+        width: 0,
+        height: 0
+      }
+    ]
   }
 
   get tenant() {
@@ -110,24 +147,12 @@ export default class extends Vue {
     return this.$route.query.auth_url
   }
 
-  btnLabels = ['确认按钮', '取消按钮']
-
-  onPreview() {
-    const preview = document.getElementById('preview')
-    preview?.scrollIntoView()
+  showAuthTemplate() {
+    (this.$refs.authtemplate as Vue & { show: Function }).show()
   }
 
-  async onSave() {
-    await runWorkflowByClass(SaveAuthPage, {
-      url: '/api/v1/tenant/a8d6968704f24c358663b5541a4c037e/app/6c8b128530ea4023bed574bb33f8d1c8/add_auth_tmpl/',
-      html: this.form.info
-    })
-    // this.initAuthPageTitle()
-    // this.initAuthPageIcon()
-    // this.initAuthPageInfo()
-    // this.initAuthPageArgeeBtn()
-    // this.initAuthPageCancelBtn()
-    // this.requestSaveAuthPage()
+  getTemplateHtml() {
+    const template = document.createElement('html')
   }
 
   initAuthPageTitle() {
@@ -150,8 +175,11 @@ export default class extends Vue {
     console.log('init')
   }
 
-  requestSaveAuthPage() {
-    console.log('requestSaveAuthPage')
+  async onSave() {
+    await runWorkflowByClass(SaveAuthPage, {
+      url: '/api/v1/tenant/a8d6968704f24c358663b5541a4c037e/app/6c8b128530ea4023bed574bb33f8d1c8/add_auth_tmpl/',
+      html: this.useTemplate ? this.getTemplateHtml() : this.html
+    })
   }
 }
 </script>
