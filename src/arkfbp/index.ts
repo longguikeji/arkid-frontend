43237e31b37a368fad601e4acb061a13701a0667
longgui-penglei <penglei@longguikeji.com>
2021-06-11 20:06:56 +0800

    fix: 🐛 request - params 参数问题

--- src/arkfbp/index.ts
+++ src/arkfbp/index.ts
@@ -121,7 +121,7 @@ export function parseStateMapping(state: any, mapping: any) {
     const item = mapping[key]
     if (typeof item === 'string') {
       tempState = getStateByStringConfig(state, item)
-      if (tempState) {
+      if (tempState !== undefined) {
         params[key] = tempState
       }
     } else if (typeof item === 'object') {
