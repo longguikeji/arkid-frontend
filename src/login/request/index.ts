5b8466ab44230689535fa2afa391ada50dbf93e6
longgui-penglei <penglei@longguikeji.com>
2021-07-01 14:04:09 +0800

    fix: 🐛 fix baseurl request

--- src/login/request/index.ts
+++ src/login/request/index.ts
@@ -1,8 +1,6 @@
 import axios from 'axios'
-import LoginStore from '../store/login'
 
 const request = axios.create({
-  baseURL: LoginStore.host,
   withCredentials: true,
   validateStatus: (status) => {
     return /^(2|3)\d{2}$/.test(String(status))
