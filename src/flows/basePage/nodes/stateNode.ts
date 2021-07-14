6ba0927dd0e05ebf94a93e907c2c9f601c3cb704
longgui-penglei <penglei@longguikeji.com>
2021-07-14 20:14:11 +0800

    fix: 🐛 fix-state-node

--- src/flows/basePage/nodes/stateNode.ts
+++ src/flows/basePage/nodes/stateNode.ts
@@ -1,11 +1,10 @@
-import OpenAPI, { ISchema } from '@/config/openapi'
+import OpenAPI, { ISchema, ITagPage, ITagPageAction, ITagUpdateAction, ITagPageOperation } from '@/config/openapi'
 import { getSchemaByPath, getInitContent } from '@/utils/schema'
 import { FunctionNode } from 'arkfbp/lib/functionNode'
 import { BasePage, IPage } from '@/flows/basePage//nodes/pageNode'
 import TableColumnState from '@/admin/common/data/Table/TableColumn/TableColumnState'
 import generateForm from '@/utils/form'
 import { generateDialogState, generateButton, addItemAction } from '@/utils/dialog'
-import { ITagPage, ITagPageAction, ITagUpdateAction } from '@/config/openapi'
 import ButtonState from '@/admin/common/Button/ButtonState'
 import whetherImportListDialog from '@/utils/list-dialog'
 import { runFlowByFile } from '@/arkfbp/index'
@@ -14,79 +13,72 @@ export class StateNode extends FunctionNode {
 
   async run() {
     const { initContent, state, currentPage, description } = this.inputs
-    const initPath = initContent.init?.path
-    const initMethod = initContent.init?.method
-    this.initPage(state, initPath, initMethod, description)
-    await this.initOperation(state, initContent, currentPage)
+    this.initPageMainState(state, initContent.init, description)
+    await this.initPageOperationState(state, initContent, currentPage)
     return this.inputs
   }
 
-  initPage(pageState: BasePage, path: string, method: string, description?: string) {
+  initPageMainState(pageState: BasePage, operation: ITagPageAction, description?: string) {
+    const { path, method } = operation
     const schema = getSchemaByPath(path, method)
     const { type, state } = pageState
-    // base page has card element
     state.card!.title = description || ''
-    // diff type page run diff init function
     switch (type) {
       case 'TablePage':
-        this.initTable(state, schema)
+        this.initTableMainState(state, schema)
         break
       case 'FormPage':
-        this.initForm(state, schema)
+        this.initFormMainState(state, schema)
         break
     }
   }
 
-  async initOperation(pageState: BasePage, initContent: ITagPage, currentPage: string) {
-    const state = pageState.state
-    if (!state.dialogs) state.dialogs = {}
-    if (initContent.page) {
-      this.initPageBtnState(pageState, initContent, currentPage)
+  async initPageOperationState(pageState: BasePage, initContent: ITagPage, currentPage: string) {
+    const { page, item } = initContent
+    if (page) {
+      this.initGlobalOperationState(pageState, page, currentPage)
     }
-    if (initContent.item) {
-      await this.initItemBtnState(pageState, initContent, currentPage)
+    if (item) {
+      await this.initLocalOperationState(pageState, item, currentPage)
     }
   }
 
-  initPageBtnState(pageState: BasePage, initContent: ITagPage, currentPage: string) {
+  initGlobalOperationState(pageState: BasePage, operations: ITagPageOperation, currentPage: string) {
     const { state, type } = pageState
-    const page = initContent.page
-    const pagekeys = Object.keys(page!)
-    for (let i = 0, len = pagekeys.length; i < len; i++) {
-      const key = pagekeys[i]
-      const action = (initContent.page![key] as ITagUpdateAction).read || initContent.page![key]
-      const { path, method } = action
+    for (const key in operations) {
+      const operation = operations[key]
+      const { path, method } = (operation as ITagUpdateAction).read || operation
+      const btn = generateButton(key, path, method, currentPage, type)
+      if (!btn) continue
+      this.addGlobalButton(state, type, btn)
       this.initDialogState(state, path, method, key, currentPage)
-      this.initCardButtons(state, key, type, path, method, currentPage)
     }
   }
 
-  async initItemBtnState(pageState: BasePage, initContent: ITagPage, currentPage: string) {
+  async initLocalOperationState(pageState: BasePage, operations: ITagPageOperation, currentPage: string) {
     const { state, type } = pageState
-    const items = initContent.item
-    if (!items) return
-    const itemkeys = Object.keys(items)
-    for (let i = 0, len = itemkeys.length; i < len; i++) {
-      const key = itemkeys[i]
-      if (key === 'children') break
-      const item = items![key]
-      if (typeof item === 'string') {
-        await this.initAppointedPage(state, type, item, key)
+    for (const key in operations) {
+      if (key === 'children') continue
+      const operation = operations[key]
+      if (typeof operation === 'string') {
+        await this.initAppointedPage(state, type, operation, key)
       } else {
-        const action = (initContent.item![key] as ITagUpdateAction).read || initContent.item![key]
-        const { path, method } = action
-        this.initDialogState(state, path, method, key, currentPage)
+        const { path, method } = (operation as ITagUpdateAction).read || operation
         if (key === 'sort') {
-          this.initSortButtons(state, action)
+          this.addSortButton(state, operation)
         } else {
-          this.initItemButtons(state, key, type, path, method, currentPage)
+          const btn = generateButton(key, path, method, currentPage, type)
+          if (!btn) continue
+          this.addLocalButton(state, type, btn)
         }
+        this.initDialogState(state, path, method, key, currentPage)
       }
     }
   }
 
   initDialogState(state: IPage, path: string, method: string, key: string, currentPage: string) {
     if (!path || !method) return
+    if (!state.dialogs) state.dialogs = {}
     const dialogState = generateDialogState(path, method, key, currentPage) // showReadOnly
     if (dialogState) {
       state.dialogs![key] = dialogState
@@ -106,7 +98,7 @@ export class StateNode extends FunctionNode {
     }
   }
 
-  initTable(state: IPage, schema: ISchema) {
+  initTableMainState(state: IPage, schema: ISchema) {
     for (const prop in schema.properties) {
       const iprop = schema.properties[prop]
       const columnState: TableColumnState = {
@@ -117,7 +109,7 @@ export class StateNode extends FunctionNode {
     }
   }
 
-  initForm(state: IPage, schema: ISchema) {
+  initFormMainState(state: IPage, schema: ISchema) {
     const showReadOnly = true, showWriteOnly = true
     const disabled = true
     const { form, forms, select } = generateForm(schema, showReadOnly, showWriteOnly, disabled)
@@ -132,63 +124,23 @@ export class StateNode extends FunctionNode {
     }
   }
 
-  // type => page type( TablePage, FormPage, TreePage )
-  initCardButtons(state: IPage, key: string, type: string, path: string, method: string, currentPage: string) {
-    const btn = generateButton(key, path, method, currentPage, type)
-    if (!btn) return
+  addGlobalButton(state: IPage, type: string, button: ButtonState) {
     if (type === 'FormPage') {
-      state.bottomButtons!.push(btn)
+      state.bottomButtons?.push(button)
     } else {
-      state.card?.buttons!.push(btn)
-    } 
+      state.card?.buttons!.push(button)
+    }
   }
 
-  // type => page type( TablePage, FormPage, TreePage )
-  initItemButtons(state: IPage, key: string, type: string, path: string, method: string, currentPage: string) {
-    const btn = generateButton(key, path, method, currentPage, type)
-    if (!btn) return
+  addLocalButton(state: IPage, type: string, button: ButtonState) {
     if (type === 'TablePage') {
-      this.initTableItemButtons(state, btn)
+      this.addTableLocalButton(state, button)
     } else if (type === 'TreePage') {
-      this.initTreeItemButtons(state, btn)
-    } else if (type === 'FormPage') {
-      state.bottomButtons?.push(btn)
+      this.addTreeLocalButton(state, button)
     }
   }
 
-  initTableItemButtons(state: IPage, btn: ButtonState) {
-    const columns = state.table!.columns
-    const len = columns?.length as number
-    if (columns![len - 1].prop !== 'actions') {
-      state.table!.columns?.push(
-        {
-          prop: 'actions',
-          label: '操作',
-          scope: {
-            type: 'ButtonArray',
-            state: []
-          }
-        }
-      )
-      columns![len].scope!.state.push(btn)
-    } else {
-      columns![len - 1].scope!.state.push(btn)
-    }
-  }
-
-  initTreeItemButtons(state: IPage, btn: ButtonState) {
-    if (!state.tree?.slot) {
-      state.tree!.slot = {
-        buttons: {
-          type: 'ButtonArray',
-          state: []
-        }
-      }
-    }
-    state.tree!.slot.buttons.state.push(btn)
-  }
-
-  initSortButtons(state: IPage, action: ITagUpdateAction | ITagPageAction) {
+  addSortButton(state: IPage, operation: ITagUpdateAction | ITagPageAction) {
     const columnSort = {
       prop: 'sort',
       label: '排序',
@@ -197,7 +149,7 @@ export class StateNode extends FunctionNode {
         state: new Array(),
       }
     }
-    Object.keys(action).forEach((sortName) => {
+    Object.keys(operation).forEach((sortName) => {
       const actionName = 'sortBy' + sortName
       if (sortName !== 'batch') {
         columnSort.scope.state.push({
@@ -212,6 +164,38 @@ export class StateNode extends FunctionNode {
     state.table?.columns?.push(columnSort)
   }
 
+  addTableLocalButton(state: IPage, button: ButtonState) {
+    const columns = state.table!.columns
+    const len = columns?.length as number
+    if (columns![len - 1].prop !== 'actions') {
+      state.table!.columns?.push(
+        {
+          prop: 'actions',
+          label: '操作',
+          scope: {
+            type: 'ButtonArray',
+            state: []
+          }
+        }
+      )
+      columns![len].scope!.state.push(button)
+    } else {
+      columns![len - 1].scope!.state.push(button)
+    }
+  }
+
+  addTreeLocalButton(state: IPage, button: ButtonState) {
+    if (!state.tree?.slot) {
+      state.tree!.slot = {
+        buttons: {
+          type: 'ButtonArray',
+          state: []
+        }
+      }
+    }
+    state.tree!.slot.buttons.state.push(button)
+  }
+
   async initAppointedPage(state: IPage, type: string, page: string, key: string) {
     const initContent = getInitContent(page)
     if (!initContent) return null
@@ -219,11 +203,11 @@ export class StateNode extends FunctionNode {
     const { path, method } = initAction as ITagPageAction
     const btn = generateButton(key, path, method, page, type)
     if (!btn) return null
-    this.initItemButtons(state, key, type, path, method, page)
+    this.addLocalButton(state, type, btn)
     addItemAction(state, path, method, key)
     const res = await runFlowByFile('flows/basePage', {
       currentPage: page,
-      initContent
+      initContent,
     })
     state.dialogs![key] = {
       state: res.state,
