1f0a699b07df2b6cde1dfc5079574728d95678fc
longgui-penglei <penglei@longguikeji.com>
2021-11-12 11:47:10 +0800

    fix: 🐛 fix init entrance node for ip problem

--- src/flows/init/nodes/entrance.ts
+++ src/flows/init/nodes/entrance.ts
@@ -5,8 +5,11 @@ import isIp from 'is-ip'
 export class Entrance extends APINode {
 
   async run() {
-    const hostIsIp = isIp(window.location.host)
-    if (!hostIsIp) { // 当前地址为非ip形式
+    let { host, port } = window.location
+    if (port !== '') {
+      host = host.substring(0, host.lastIndexOf(':'))
+    }
+    if (!isIp(host)) { // 当前地址为非ip形式
       this.url = '/api/v1/get_frontendurl/'
       this.method = 'GET'
       const outputs = await super.run()
