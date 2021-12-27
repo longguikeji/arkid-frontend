9c4ee28220a3d7ab909fa041ff98391ffe05eb2e
longgui-penglei <penglei@longguikeji.com>
2021-12-27 18:19:00 +0800

    feat: 🎸 backend auth

--- src/flows/init/nodes/entrance.ts
+++ src/flows/init/nodes/entrance.ts
@@ -1,5 +1,6 @@
 import { APINode } from '@/arkfbp/nodes/apiNode'
 import { ConfigModule } from '@/store/modules/config'
+import { setToken } from '@/utils/auth'
 
 export class EntranceNode extends APINode {
   async run() {
@@ -9,5 +10,12 @@ export class EntranceNode extends APINode {
     const outputs = await super.run()
     const url = outputs?.url
     if (url) ConfigModule.setOrigin(url)
+
+    // get backend auth info
+    this.url = '/api/v1/backend_auth'
+    this.method = 'get'
+    const data = await super.run()
+    const token = data?.token
+    if (token) setToken(token)
   }
 }
