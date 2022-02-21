e82266d4dc5c112a12fd0db8ff1766dc780c5422
longgui-penglei <penglei@longguikeji.com>
2022-02-21 19:59:50 +0800

    feat: 🎸 dynamic icon and title

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -1,18 +1,32 @@
-
 // Check if an element has a class
 export const hasClass = (ele: HTMLElement, className: string) => {
-  return !!ele.className.match(new RegExp('(\\s|^)' + className + '(\\s|$)'))
-}
+  return !!ele.className.match(new RegExp("(\\s|^)" + className + "(\\s|$)"));
+};
 
 // Add class to element
 export const addClass = (ele: HTMLElement, className: string) => {
-  if (!hasClass(ele, className)) ele.className += ' ' + className
-}
+  if (!hasClass(ele, className)) ele.className += " " + className;
+};
 
 // Remove class from element
 export const removeClass = (ele: HTMLElement, className: string) => {
   if (hasClass(ele, className)) {
-    const reg = new RegExp('(\\s|^)' + className + '(\\s|$)')
-    ele.className = ele.className.replace(reg, ' ')
+    const reg = new RegExp("(\\s|^)" + className + "(\\s|$)");
+    ele.className = ele.className.replace(reg, " ");
   }
-}
+};
+
+// 动态修改当前浏览器的BOM-title
+export const updateTitle = (title: string) => {
+  document.title = title;
+};
+
+// 动态修改当前浏览器的BOM-icon
+export const updateIcon = (href: string = "") => {
+  const el = document.createElement("link");
+  if (!el) return;
+  el.setAttribute("type", "image/x-icon");
+  el.setAttribute("rel", "shortcut icon");
+  el.setAttribute("href", href || "./favicon.ico");
+  document.getElementsByTagName("head")[0].appendChild(el);
+};
--- vue.config.js
+++ vue.config.js
@@ -27,15 +27,6 @@ module.exports = {
     },
     disableHostCheck: true
   },
-  pwa: {
-    iconPaths: {
-      favicon32: 'favicon.ico',
-      favicon16: 'favicon.ico',
-      appleTouchIcon: 'favicon.ico',
-      maskIcon: 'favicon.ico',
-      msTileImage: 'favicon.ico'
-    }
-  },
   pluginOptions: {
     'style-resources-loader': {
       preProcessor: 'scss',
