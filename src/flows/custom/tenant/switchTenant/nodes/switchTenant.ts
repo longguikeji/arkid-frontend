b11750b30a9099087c23380a0072ad7f857e0d63
longgui-penglei <penglei@longguikeji.com>
2022-02-21 20:08:16 +0800

    style:💄 format

--- src/flows/custom/tenant/switchTenant/nodes/switchTenant.ts
+++ src/flows/custom/tenant/switchTenant/nodes/switchTenant.ts
@@ -1,39 +1,39 @@
-import { FunctionNode } from "arkfbp/lib/functionNode";
-import { getToken } from "@/utils/auth";
-import getBaseUrl from "@/utils/get-base-url";
-import { ConfigModule } from "@/store/modules/config";
-import { TenantModule } from "@/store/modules/tenant";
+import { FunctionNode } from 'arkfbp/lib/functionNode'
+import { getToken } from '@/utils/auth'
+import getBaseUrl from '@/utils/get-base-url'
+import { ConfigModule } from '@/store/modules/config'
+import { TenantModule } from '@/store/modules/tenant'
 
 export class SwitchTenant extends FunctionNode {
   async run() {
-    const { client } = this.inputs;
-    const tenant = client.data;
-    if (!tenant) return;
-    const { origin, desktop } = ConfigModule;
-    const path = desktop.visible ? "/desktop" : "/mine";
-    const { slug, use_slug } = tenant;
-    TenantModule.changeCurrentTenant(tenant);
-    let url = "";
+    const { client } = this.inputs
+    const tenant = client.data
+    if (!tenant) return
+    const { origin, desktop } = ConfigModule
+    const path = desktop.visible ? '/desktop' : '/mine'
+    const { slug, use_slug } = tenant
+    TenantModule.changeCurrentTenant(tenant)
+    let url = ''
     if (slug && use_slug) {
-      ConfigModule.setSlug(slug);
+      ConfigModule.setSlug(slug)
       url =
         origin.replace(
-          window.location.protocol + "//",
-          window.location.protocol + "//" + slug + "."
+          window.location.protocol + '//',
+          window.location.protocol + '//' + slug + '.',
         ) +
         path +
         getBaseUrl() +
-        "?token=" +
-        getToken();
-      window.location.replace(url);
+        '?token=' +
+        getToken()
+      window.location.replace(url)
     } else {
-      ConfigModule.setSlug();
+      ConfigModule.setSlug()
       url =
         origin +
         path +
         getBaseUrl() +
-        `?tenant=${TenantModule.currentTenant.uuid}&token=${getToken()}`;
+        `?tenant=${TenantModule.currentTenant.uuid}&token=${getToken()}`
     }
-    window.location.replace(url);
+    window.location.replace(url)
   }
 }
--- src/flows/init/nodes/tenant.ts
+++ src/flows/init/nodes/tenant.ts
@@ -1,57 +1,57 @@
-import { APINode } from "@/arkfbp/nodes/apiNode";
-import { TenantModule } from "@/store/modules/tenant";
-import { getSlug, getUrlParamByName, isIPAddress } from "@/utils/url";
-import { ConfigModule } from "@/store/modules/config";
-import { updateTitle, updateIcon } from "@/utils";
+import { APINode } from '@/arkfbp/nodes/apiNode'
+import { TenantModule } from '@/store/modules/tenant'
+import { getSlug, getUrlParamByName, isIPAddress } from '@/utils/url'
+import { ConfigModule } from '@/store/modules/config'
+import { updateTitle, updateIcon } from '@/utils'
 
 export class TenantNode extends APINode {
   async run() {
     // current tenant uuid and tenant switch info
-    let uuid = "";
+    let uuid = ''
 
     // get method
-    this.method = "get";
+    this.method = 'get'
 
     // get platform uuid and tenant switch info
-    this.url = "/api/v1/tenant_switchinfo/";
-    const outputs = await super.run();
+    this.url = '/api/v1/tenant_switchinfo/'
+    const outputs = await super.run()
     if (outputs) {
-      uuid = outputs.platform_tenant_uuid;
-      TenantModule.setTenantSwitch(outputs.switch);
+      uuid = outputs.platform_tenant_uuid
+      TenantModule.setTenantSwitch(outputs.switch)
     }
 
     // get current slug
-    const slug = getSlug();
-    if (slug === "" || isIPAddress()) {
+    const slug = getSlug()
+    if (slug === '' || isIPAddress()) {
       uuid =
-        getUrlParamByName("tenant") || getUrlParamByName("tenant_uuid") || uuid;
+        getUrlParamByName('tenant') || getUrlParamByName('tenant_uuid') || uuid
       if (uuid) {
-        uuid = uuid.replace(/-/g, "");
-        this.url = `/api/v1/tenant/${uuid}/`;
-        const res = await super.run();
+        uuid = uuid.replace(/-/g, '')
+        this.url = `/api/v1/tenant/${uuid}/`
+        const res = await super.run()
         if (res?.uuid) {
-          TenantModule.setTenantIsPlatform(res.uuid === uuid);
-          TenantModule.changeCurrentTenant(res);
+          TenantModule.setTenantIsPlatform(res.uuid === uuid)
+          TenantModule.changeCurrentTenant(res)
         } else {
-          TenantModule.changeCurrentTenant({ uuid });
+          TenantModule.changeCurrentTenant({ uuid })
         }
       }
     } else {
-      this.url = `/api/v1/tenant/${slug}/slug/`;
-      const data = await super.run();
+      this.url = `/api/v1/tenant/${slug}/slug/`
+      const data = await super.run()
       if (data?.uuid) {
-        TenantModule.setTenantIsPlatform(data.uuid === uuid);
-        ConfigModule.setSlug(slug);
-        TenantModule.changeCurrentTenant(data);
+        TenantModule.setTenantIsPlatform(data.uuid === uuid)
+        ConfigModule.setSlug(slug)
+        TenantModule.changeCurrentTenant(data)
       } else {
-        TenantModule.changeCurrentTenant({ uuid });
+        TenantModule.changeCurrentTenant({ uuid })
       }
     }
 
-    const { name, icon } = TenantModule.currentTenant;
+    const { name, icon } = TenantModule.currentTenant
     if (name) {
-      updateTitle(name);
+      updateTitle(name)
     }
-    updateIcon(icon);
+    updateIcon(icon)
   }
 }
